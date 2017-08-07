2e7ad434c7babf747677903e03d4dfb8a635cdac
sravan <sravan@meetmastiff.com>
2017-08-07 00:32:24 -0700

    fix path

--- src/start.sh
+++ src/start.sh
@@ -4,9 +4,9 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 sleep 1
 
-chmod +x usr/src/app/src/mastiff_ap.sh
-sh usr/src/app/src/mastiff_ap.sh
-sh usr/src/app/src/create_user.sh
+chmod +x /usr/src/app/src/mastiff_ap.sh
+sh /usr/src/app/src/mastiff_ap.sh
+sh /usr/src/app/src/create_user.sh
 
 # Start sshd if we don't use the init system
 if [ "$INITSYSTEM" != "on" ]; then
