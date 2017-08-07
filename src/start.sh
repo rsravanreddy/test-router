0c829bf48c5fe900b9fc410c7c1453d241efb62a
sravan <sravan@meetmastiff.com>
2017-08-07 00:36:06 -0700

    use bash instead of sh

--- src/start.sh
+++ src/start.sh
@@ -5,8 +5,8 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 sleep 1
 
 chmod +x /usr/src/app/src/mastiff_ap.sh
-sh /usr/src/app/src/mastiff_ap.sh
-sh /usr/src/app/src/create_user.sh
+bash /usr/src/app/src/mastiff_ap.sh
+bash /usr/src/app/src/create_user.sh
 
 # Start sshd if we don't use the init system
 if [ "$INITSYSTEM" != "on" ]; then
