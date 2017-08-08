7bf79b6510fc4964370356dc12c127a0f256d16e
sravan <sravan@meetmastiff.com>
2017-08-07 21:48:21 -0700

    try in start.sh

--- src/start.sh
+++ src/start.sh
@@ -3,8 +3,8 @@
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 sleep 1
-#chmod +x /usr/src/app/src/mastiff_ap.sh
-#bash /usr/src/app/src/mastiff_ap.sh
+chmod +x /usr/src/app/src/mastiff_ap.sh
+bash /usr/src/app/src/mastiff_ap.sh
 #bash /usr/src/app/src/create_user.sh
 
 # Start sshd if we don't use the init system
