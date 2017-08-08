b8223dabf641130ebbe4ddd8de2451e835c292cb
sravan <sravan@meetmastiff.com>
2017-08-07 22:00:18 -0700

    start access point script from main script

--- src/start.sh
+++ src/start.sh
@@ -3,14 +3,15 @@
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 sleep 1
-chmod +x /usr/src/app/src/mastiff_ap.sh
-bash /usr/src/app/src/mastiff_ap.sh
 #bash /usr/src/app/src/create_user.sh
 
 # Start sshd if we don't use the init system
 if [ "$INITSYSTEM" != "on" ]; then
   /usr/sbin/sshd -p 22 &
 fi
+
+chmod +x /usr/src/app/src/mastiff_ap.sh
+bash /usr/src/app/src/mastiff_ap.sh
 
 echo "This is where your application would start..."
 while : ; do
